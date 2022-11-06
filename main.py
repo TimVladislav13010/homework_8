1fe86b9c31d1e1cc3e92d5e4b2adcc100639b424
TimVladislav13010 <vladislav130ua1@gmail.com>
2022-11-06 09:19:12 +0200

    append USERS

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-pass
+from datetime import datetime
+
+
+USERS = [
+    {"name": "name_1", "birthday": datetime(year=2022, month=11, day=5).date()},
+    {"name": "name_2", "birthday": datetime(year=2022, month=11, day=6).date()},
+    {"name": "name_3", "birthday": datetime(year=2022, month=11, day=7).date()},
+    {"name": "name_4", "birthday": datetime(year=2022, month=11, day=8).date()},
+    {"name": "name_5", "birthday": datetime(year=2022, month=11, day=9).date()},
+    {"name": "name_6", "birthday": datetime(year=2022, month=11, day=10).date()},
+    {"name": "name_7", "birthday": datetime(year=2022, month=11, day=11).date()},
+    {"name": "name_8", "birthday": datetime(year=2022, month=11, day=12).date()},
+    {"name": "name_9", "birthday": datetime(year=2022, month=11, day=13).date()},
+]
+
+print(USERS)
